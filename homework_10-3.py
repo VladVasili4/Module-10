55a3c9b57b00df9e3338f1d4a8d8b3a6ad3393a2
VladVasili4 <dipol777@ya.ru>
2024-08-04 12:30:10 +0300

    Add files via upload

--- homework_10-3.py
+++ homework_10-3.py
@@ -1,26 +1,81 @@
+"""
+Цель задания:
+Практически применить знания о механизмах блокировки потоков в Python, используя класс Lock из модуля threading.
+
+Задание:
+Реализуйте программу, которая имитирует доступ к общему ресурсу с использованием механизма блокировки потоков.
+
+Класс BankAccount должен отражать банковский счет с балансом и методами для пополнения и снятия денег. Необходимо
+использовать механизм блокировки, чтобы избежать проблемы гонок (race conditions) при модификации общего ресурса.
+
+Пример работы:
+def deposit_task(account, amount):
+  for _ in range(5):
+    account.deposit(amount)
+
+def withdraw_task(account, amount):
+  for _ in range(5):
+    account.withdraw(amount)
+account = BankAccount()
+
+deposit_thread = threading.Thread(target=deposit_task, args=(account, 100))
+withdraw_thread = threading.Thread(target=withdraw_task, args=(account, 150))
+
+deposit_thread.start()
+withdraw_thread.start()
+
+deposit_thread.join()
+withdraw_thread.join()
+
+Вывод в консоль:
+Deposited 100, new balance is 1100
+Deposited 100, new balance is 1200
+Deposited 100, new balance is 1300
+Deposited 100, new balance is 1400
+Deposited 100, new balance is 1500
+Withdrew 150, new balance is 1350
+Withdrew 150, new balance is 1200
+Withdrew 150, new balance is 1050
+Withdrew 150, new balance is 900
+Withdrew 150, new balance is 750
+
+Примечание:
+Используйте класс Lock из модуля threading для блокировки доступа к общему ресурсу.
+Ожидается создание двух потоков, один для пополнения счета, другой для снятия денег.
+Используйте with (lock object): в начале каждого метода, чтобы использовать блокировку
+"""
+
+
 from threading import Thread, Lock
 
 lock = Lock()
-class BankAccount:
-    bankAcc = 1000
 
-    def __init__(self, accaunt):
-        self.accaunt = accaunt
+class BankAccount(Thread):
+    balance = 1000
 
 
-    def deposit_task(self, account, amount):
+    def __init__(self):
+        super().__init__()
+
+    def deposit(self, amount):
         self.amount = amount
-        global bankAcc
-        for i in range(5):
-            with lock:
-                bankAcc += self.amount
+        with lock:
+            self.balance += self.amount
+            print(f'Deposited {self.amount}, new balance is {self.balance}')
 
+    def withdraw(self, amount):
+        self.amount = amount
+        with lock:
+            self.balance -= self.amount
+            print(f'Withdrew {self.amount}, new balance is {self.balance}')
 
+def deposit_task(account, amount):
+    for i in range(5):
         account.deposit(amount)
 
-    def withdraw_task(self, account, amount):
-        for _ in range(5):
-            account.withdraw(amount)
+def withdraw_task(account, amount):
+    for i in range(5):
+        account.withdraw(amount)
 
 
 account = BankAccount()
