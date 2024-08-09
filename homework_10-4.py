4916189314406a526727c853cf5d666f987ee9a5
VladVasili4 <dipol777@ya.ru>
2024-08-09 17:24:21 +0300

    Add files via upload

--- homework_10-4.py
+++ homework_10-4.py
@@ -6,7 +6,10 @@ import queue
 class Table:
     def __init__(self, number):
         self.number = number
-        self.guest = None
+        self.guest = False
+        # for t in tables:
+        #     self.table = t.number
+        #     print(f'Cтолик в кафе :{self.table}')
 
 
 
@@ -18,35 +21,38 @@ class Guest(Thread):
     def run(self):
         t = random.randint(3, 11)
         sleep(t)
-        print(f'Выполняется поток {self.name} ')
+        print(f' {self.name} сидит, ест.')
 
 
 queue = queue.Queue
 class Cafe():
-    def __init__(self, tables):
-        # self.table = Table(number)
-        for t in tables:
-            self.table = t.number
-            print(f'Cтолик в кафе :{self.table}')
+
+    def __init__(self, tables, queue):
+        self.tables = tables
+        self.queue  = queue
 
 
-    def guest_arrival(self, *guest ):                      #  (прибытие гостей)
+    def guest_arrival(self, *guests ):                      #  (прибытие гостей)
         self.guests = guests
-        for g in self.guests:
-            g.start()
-            print(f"{g} сел(-а) за стол номер {self.table}")
-        # queue.put()
+        for t in self.tables:
+            if t.guest == False:
+                for g in self.guests:
+                    g.start()
+                    t.guest = g
+                    print(f"{g.name} сел(-а) за стол номер {t}")
+                else: queue.put(item=self.g)
 
 #
     def discuss_guests(self):                      # (обслужить гостей)
         while not queue.empty:
+            # if self.
             for i in guests:
                 i.join()
                 print(f'Поел: {i}')
-            # queue.get()
+            self.queue.get(i)
 
 # Создание столов
-tables = [Table(number) for number in range(1, 6)]
+tables = [Table(number) for number in range(1, 6)]          # Это генератор списка!!!
 print(f'Столики с номерами {tables} ')
 
 
@@ -56,7 +62,7 @@ guests_names = [
 'Vitoria', 'Nikita', 'Galina', 'Pavel', 'Ilya', 'Alexandra'
 ]
 # Создание гостей
-guests = [Guest(name) for name in guests_names]
+guests = [Guest(name) for name in guests_names]             # Это генератор списка!!!
 print(f'Гости : {guests}')
 # for g in guests:
 #     g.start()
@@ -66,7 +72,7 @@ print(f'Гости : {guests}')
 #     print(f'Поел: {i}')
 
 # Заполнение кафе столами
-cafe = Cafe(tables)
+cafe = Cafe(tables, queue)
 # Приём гостей
 cafe.guest_arrival(*guests)
 # Обслуживание гостей
